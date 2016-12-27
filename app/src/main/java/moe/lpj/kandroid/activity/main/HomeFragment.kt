8ac23a651a940709393da8ecfa2c786a32d91342
Dexlind <lgzh1215@gmail.com>
2016-12-27 17:49:06 +0800

    add

--- app/src/main/java/moe/lpj/kandroid/activity/main/HomeFragment.kt
+++ app/src/main/java/moe/lpj/kandroid/activity/main/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.app_bar_main.*
-
 import moe.lpj.kandroid.R
 
 class HomeFragment : Fragment() {
@@ -21,6 +20,8 @@ class HomeFragment : Fragment() {
         super.onAttach(context)
         if (context is MainActivity) {
             mActivity = context
+//            addTabLayout()
+            showTab()
         } else {
             throw RuntimeException("???")
         }
@@ -31,33 +32,27 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        setUpTabLayout()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        removeTabLayout()
-    }
-
     override fun onDetach() {
         super.onDetach()
+        removeTabLayout()
         mActivity = null
     }
 
-    private fun setUpTabLayout() {
+    private fun addTabLayout() {
         if (tabLayout != null) return
-        val app_bar_layout = mActivity?.app_bar_layout ?: return
+        val app_bar_layout = mActivity!!.app_bar_layout ?: return
         val tabLayout: TabLayout = TabLayout(app_bar_layout.context)
         tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
         tabLayout.tabGravity = TabLayout.GRAVITY_CENTER
-        tabLayout.elevation = 4.0f
         app_bar_layout.addView(tabLayout)
     }
 
     private fun removeTabLayout() {
         mActivity!!.app_bar_layout.removeView(tabLayout)
         tabLayout = null
+    }
+
+    private fun showTab() {
+        mActivity!!.tab_layout.visibility = View.VISIBLE
     }
 }
---
+++ app/src/main/java/moe/lpj/kandroid/viewmodel/Mission.java
@@ -0,0 +1,12 @@
+package moe.lpj.kandroid.viewmodel;
+
+import android.databinding.BaseObservable;
+
+public class Mission extends BaseObservable {
+
+
+
+    public boolean hasData() {
+        return false;
+    }
+}
